16aea632177daab798c0ed99ac7a83941688ccb0
Sriram Yadagiri <73784283+SriramYadagiri@users.noreply.github.com>
2026-04-05 10:51:43 -0500

    changed questions

--- src/data/rounds.ts
+++ src/data/rounds.ts
@@ -18,6 +18,16 @@ export const rounds: Round[] = [
     round: 1,
     questions: [
       {
+        question: "Name something a woman changes that a man might not notice.",
+        answers: [
+          { text: "Hair", points: 35 },
+          { text: "Makeup", points: 25 },
+          { text: "Nails", points: 20 },
+          { text: "Perfume", points: 15 },
+          { text: "Clothes", points: 5 },
+        ],
+      },
+      {
         question: "Name something a married couple commonly fight about",
         answers: [
           { text: "Finances", points: 35 },
@@ -48,16 +58,6 @@ export const rounds: Round[] = [
         ],
       },
       {
-        question: "Name something a married couple might like about being married",
-        answers: [
-          { text: "Companionship / friendship", points: 35 },
-          { text: "Emotional support", points: 25 },
-          { text: "Physical relationship", points: 20 },
-          { text: "Security", points: 15 },
-          { text: "Not being single", points: 5 },
-        ],
-      },
-      {
         question: "Name something a spouse might do to show their love",
         answers: [
           { text: "Hug / kiss", points: 35 },
@@ -75,46 +75,6 @@ export const rounds: Round[] = [
           { text: "Making joint decisions", points: 20 },
           { text: "Sharing", points: 15 },
           { text: "Extra responsibility", points: 5 },
-        ],
-      },
-      {
-        question: "Name a perfect married couple (past or present)",
-        answers: [
-          { text: "Barack & Michelle Obama", points: 35 },
-          { text: "Blake Lively & Ryan Reynolds", points: 25 },
-          { text: "Meghan Markle & Prince Harry", points: 20 },
-          { text: "Beyonce & Jay Z", points: 15 },
-          { text: "Alex Rodriguez & Jennifer Lopez", points: 5 },
-        ],
-      },
-      {
-        question: "Name a place where people get married",
-        answers: [
-          { text: "Church", points: 35 },
-          { text: "Temple", points: 25 },
-          { text: "Las Vegas", points: 20 },
-          { text: "Paris", points: 15 },
-          { text: "Beach", points: 5 },
-        ],
-      },
-      {
-        question: "Name a great gift to give to newlyweds",
-        answers: [
-          { text: "Money", points: 35 },
-          { text: "Gift cards", points: 25 },
-          { text: "Games", points: 20 },
-          { text: "Break / vacation", points: 15 },
-          { text: "Advice", points: 5 },
-        ],
-      },
-      {
-        question: "Name a gift a newly married couple may receive multiples of",
-        answers: [
-          { text: "Towels", points: 35 },
-          { text: "Perfume", points: 25 },
-          { text: "Toaster", points: 20 },
-          { text: "Dishes", points: 15 },
-          { text: "DIY tools", points: 5 },
         ],
       },
       {
@@ -158,16 +118,6 @@ export const rounds: Round[] = [
         ],
       },
       {
-        question: "What is the most important thing to consider when planning a date",
-        answers: [
-          { text: "Time", points: 35 },
-          { text: "Babysitter", points: 25 },
-          { text: "Cost", points: 20 },
-          { text: "Your interests", points: 15 },
-          { text: "Distance", points: 5 },
-        ],
-      },
-      {
         question: "Name something you might do on a romantic day with your partner",
         answers: [
           { text: "Picnic", points: 35 },
@@ -188,16 +138,6 @@ export const rounds: Round[] = [
         ],
       },
       {
-        question: "Name the most important thing a couple has to pick for the wedding",
-        answers: [
-          { text: "Venue", points: 35 },
-          { text: "Rings", points: 25 },
-          { text: "Date", points: 20 },
-          { text: "Food", points: 15 },
-          { text: "Location / spot", points: 5 },
-        ],
-      },
-      {
         question: "Name a party that is centered around a couple",
         answers: [
           { text: "Anniversary party", points: 35 },
@@ -206,37 +146,7 @@ export const rounds: Round[] = [
           { text: "Bridal shower", points: 15 },
           { text: "Baby shower", points: 5 },
         ],
-      },
-      {
-        question: "Name a food that is associated with romance",
-        answers: [
-          { text: "Strawberries", points: 35 },
-          { text: "Whipped cream", points: 25 },
-          { text: "Chocolate", points: 20 },
-          { text: "Oysters", points: 15 },
-          { text: "Pasta", points: 5 },
-        ],
-      },
-      {
-        question: "Name something you and your spouse might share on a date",
-        answers: [
-          { text: "Dessert", points: 35 },
-          { text: "Meal", points: 25 },
-          { text: "Popcorn", points: 20 },
-          { text: "A kiss", points: 15 },
-          { text: "Stories", points: 5 },
-        ],
-      },
-      {
-        question: "Name advice newlyweds often receive",
-        answers: [
-          { text: "Don't go to bed angry", points: 35 },
-          { text: "Keep dating each other", points: 25 },
-          { text: "Budget your money", points: 20 },
-          { text: "Have lots of babies", points: 15 },
-          { text: "Put the other first", points: 5 },
-        ],
-      },
+      }
     ],
   },
 ];
